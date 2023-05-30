8b57edad3371e962f0ec81a7db00451ce44dad89
ayo <oji@applogicx.com>
2023-05-30 15:43:13 -0600

    CommentList

--- components/blog/BlogPost.js
+++ components/blog/BlogPost.js
@@ -8,32 +8,60 @@ import { useState, useEffect } from "react";
 
 const BlogPost = ({ post, currentUser }) => {
   const [comments, setComments] = useState([]);
+  const fetchComments = async (postId) => {
+    try {
+      const { data: comments, error: commentsError } = await supabase
+        .from("blog_comments")
+        .select("id, post_id, content, created_at, user_id")
+        .eq("post_id", postId);
+  
+      if (commentsError) {
+        console.error("Error fetching comments:", commentsError.message);
+        throw commentsError;
+      }
+  
+      const user_ids = comments.map((comment) => comment.user_id);
+  
+      const { data: users, error: usersError } = await supabase
+        .from("profiles").select("user_id, name")
+        .in("user_id", user_ids);
+  
+      if (usersError) {
+        console.error("Error fetching user names:", usersError.message);
+        throw usersError;
+      }
+  
+      let commentsWithUserNames = comments.map((comment) => {
+        return {
+          ...comment,
+          name: users.find((user) => user.user_id === comment.user_id)?.name,
+        };
+      });
+  
+      return commentsWithUserNames;
+    } catch (error) {
+      console.error("Error fetching comments:", error.message);
+      return [];
+    }
+  };
 
   useEffect(() => {
-    const fetchComments = async () => {
-      const { data, error } = await supabase
-        .from("blog_comments")
-        .select("*, user_id (name)")
-        .eq("post_id", post.id);
-  
-      if (error) {
+    fetchComments(post.id)
+      .then((fetchedComments) => {
+        console.log("Fetched comments:", fetchedComments);
+        setComments(fetchedComments);
+      })
+      .catch((error) => {
         console.error("Error fetching comments:", error.message);
-      } else {
-        setComments(data);
-      }
-    };
-  
-    fetchComments();
+      });
   }, [post.id]);
+
+  console.log('Rendering BlogPost component');
   
   return (
     <div className="blog-post">
       <h1>{post.title}</h1>
-      <ReactQuill
-        value={post.content}
-        readOnly={true}
-        theme="snow"
-      />
+      <ReactQuill value={post.content} readOnly={true} theme="snow" />
       <hr />
       <CommentList comments={comments} />
       {currentUser && <CommentForm post={post} currentUser={currentUser} />}
--- components/blog/CommentList.js
+++ components/blog/CommentList.js
@@ -1,12 +1,11 @@
-
-
 const CommentList = ({ comments }) => {
-    return (
-      <div>
-        {comments.map((comment) => (
-          <div key={comment.id} className="comment">
-          <h4>{comment.user.name}</h4>
+  return (
+    <div>
+      {comments.map((comment) => (
+        <div key={comment.id} className="comment">
           <p>{comment.content}</p>
+          <h5>{comment.name}</h5>
+
           <span>{new Date(comment.created_at).toLocaleString()}</span>
         </div>
       ))}
