a9d45e0e060192bcc7459d1b0f0d38f2ec7d7770
ayooji <oji@applogicx.com>
2024-07-23 15:22:28 -0600

    commit stripe api to live

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
- 
+  apiVersion: '2020-08-27',
 });
 
 export default async function handler(req, res) {
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         line_items: [
           {
-            price: 'price_1PeOnhAlS7L0SIaxmeOJJnDV', // Use the provided price ID
+            price: 'price_1Pfq6dAlS7L0SIaxT4d4yXd1', // Use the provided price ID
             quantity: 1,
           },
         ],
